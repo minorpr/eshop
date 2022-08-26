e4012a1df6d10062499a21da7c2848e1ce789b58
minorpr <projectminor630@gmail.com>
2022-08-26 11:45:31 +0530

    pdtdisplay1

---
+++ src/app/instant/instant.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-instant',
+  templateUrl: './instant.component.html',
+  styleUrls: ['./instant.component.css']
+})
+export class InstantComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
